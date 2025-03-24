2639777ea21dd0c25b85797940c847f3c484b40e
larrykeck13 <101larrykeck@gmail.com>
2025-03-23 22:31:27 -0400

    Added initial html css and scripts

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+// scripts.js - You can add your custom JavaScript here
+// Example: Smooth scroll for anchors
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+  
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
