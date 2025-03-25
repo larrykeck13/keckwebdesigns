28a1b3ebdb0fa7730518f30f502b8f90788cc55f
larrykeck13 <101larrykeck@gmail.com>
2025-03-25 01:13:18 -0400

    readd script

--- js/scripts.js
+++ js/scripts.js
@@ -10,27 +10,27 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
   });
 
-// document.querySelector('form').addEventListener('submit', function(event) {
-//   event.preventDefault();
+document.querySelector('form').addEventListener('submit', function(event) {
+  event.preventDefault();
   
-//   var form = event.target;
-//   var formData = new FormData(form);
+  var form = event.target;
+  var formData = new FormData(form);
   
-//   fetch(form.action, {
-//       method: form.method,
-//       body: formData,
-//       headers: {
-//           'Accept': 'application/json'
-//       }
-//   }).then(response => {
-//       if (response.ok) {
-//           var successModal = new bootstrap.Modal(document.getElementById('successModal'));
-//           successModal.show();
-//           form.reset();
-//       } else {
-//           alert('There was an error submitting the form. Please try again.');
-//       }
-//   }).catch(error => {
-//       alert('There was an error submitting the form. Please try again.');
-//   });
-// });
+  fetch(form.action, {
+      method: form.method,
+      body: formData,
+      headers: {
+          'Accept': 'application/json'
+      }
+  }).then(response => {
+      if (response.ok) {
+          var successModal = new bootstrap.Modal(document.getElementById('successModal'));
+          successModal.show();
+          form.reset();
+      } else {
+          alert('There was an error submitting the form. Please try again.');
+      }
+  }).catch(error => {
+      alert('There was an error submitting the form. Please try again.');
+  });
+});
